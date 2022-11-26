5cd18c641619e078baa8bb9319033ebb30c9bd4e
Michel Mexique <59715170+michel-mexique@users.noreply.github.com>
2022-11-26 01:30:43 -0500

    Few changes

--- datasette_launch_script.py
+++ datasette_launch_script.py
@@ -1,9 +1,9 @@
 import os
 cmd = os.system
 
-"""
-Docker installaton process
-"""
+print("######   DON'T FORGET TO SET NGROK AUTHTOKEN ENV VARIABLE   ######")
+print("######   WITH COMMAND 'export NGROK_AUTHTOKEN=TOKEN'   ######")
+# 1) Docker installaton process
 
 cmd('sudo apt-get remove docker docker-engine docker.io containerd runc')
 cmd('sudo apt-get update')
@@ -23,3 +23,22 @@ echo \
 """)
 cmd('sudo apt-get update')
 cmd('sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin')
+cmd('sudo docker run hello-world')
+
+
+# 2) NGrok Installation process
+
+cmd('sudo snap install ngrok')
+cmd('ngrok config add-authtoken ' + os.environ['NGROK_AUTHTOKEN'])
+
+# 3) Datasette deployement on local domain
+
+cmd("""
+docker run -p 8001:8001 -v `pwd`:/mnt \
+    datasetteproject/datasette \
+    datasette -p 8001 -h 0.0.0.0 mnt/database.db
+""")
+
+# 4) Web deployment on ngrok
+
+cmd('ngrok http 8001')
